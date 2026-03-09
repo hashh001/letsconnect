50291c46c44d4577328ff2c624805d1b6d836f3b
hashh001 <ggarg8277@gmail.com>
2026-03-09 15:08:08 +0530

    fix: corrected HTML form structure for private group link

---
+++ test_db.js
@@ -0,0 +1,4 @@
+﻿const admin = require('firebase-admin');
+
+// Since we don't have the service account key locally for a quick read, we'll just check the frontend logic again
+console.log('Using node to check frontend logic syntax for private link storage');
